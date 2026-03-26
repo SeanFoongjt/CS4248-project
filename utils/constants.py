3eaa9fb91a97ce1e27e4ea2a6a778b0687e151be
notnotmax <156508404+notnotmax@users.noreply.github.com>
2026-03-26 21:04:59 +0800

    Revise entity masks

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,27 @@
 # masks for named entity recognition
-ENTITY_MASKS = ["PERSON", "ORG", "GPE", "NORP", "PRODUCT", "DATE", "MONEY"]
+
+# list of all possible tag options for reference, from https://github.com/explosion/spaCy/discussions/9147
+
+# PERSON:      People, including fictional.
+# NORP:        Nationalities or religious or political groups.
+# FAC:         Buildings, airports, highways, bridges, etc.
+# ORG:         Companies, agencies, institutions, etc.
+# GPE:         Countries, cities, states.
+# LOC:         Non-GPE locations, mountain ranges, bodies of water.
+# PRODUCT:     Objects, vehicles, foods, etc. (Not services.)
+# EVENT:       Named hurricanes, battles, wars, sports events, etc.
+# WORK_OF_ART: Titles of books, songs, etc.
+# LAW:         Named documents made into laws.
+# LANGUAGE:    Any named language.
+# DATE:        Absolute or relative dates or periods.
+# TIME:        Times smaller than a day.
+# PERCENT:     Percentage, including ”%“.
+# MONEY:       Monetary values, including unit.
+# QUANTITY:    Measurements, as of weight or distance.
+# ORDINAL:     “first”, “second”, etc.
+# CARDINAL:    Numerals that do not fall under another type.
+
+ENTITY_MASKS = ["PERSON", "NORP", "FAC", "ORG", "GPE", "LOC"]
 
 # mappings for article section standardisation
 SECTION_MAPPINGS = {

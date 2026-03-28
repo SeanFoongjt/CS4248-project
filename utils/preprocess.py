e1d4627d41b984f63cd583a6571cf32a33d65433
notnotmax <156508404+notnotmax@users.noreply.github.com>
2026-03-28 16:55:50 +0800

    Add workflow for SoC cluster

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,9 +3,7 @@ import nltk
 import re
 import spacy
 from utils.constants import SECTION_MAPPINGS, ENTITY_MASKS
-from nltk.corpus import stopwords, wordnet
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+
 
 nlp = spacy.load("en_core_web_trf") # might take a while
 
@@ -65,12 +63,35 @@ def preprocess_description(text: str, masks: list = ENTITY_MASKS):
     return processed_text
 
 
+def preprocess_description_from_doc(doc, masks=ENTITY_MASKS):
+    """
+    Modified version that accepts a spaCy Doc instead of a string.
+    Used for efficiency when running on SoC.
+    """
+    text = doc.text
+
+    text = re.sub(r'^[A-Z\s,.]+—', '', text).strip()
+    
+    sentences = list(doc.sents)
+    processed_text = " ".join([s.text for s in sentences])
+    
+    for ent in reversed(doc.ents):
+        if ent.label_ in masks and ent.end <= sentences[-1].end:
+            processed_text = (processed_text[:ent.start_char] + f"[{ent.label_}]" + processed_text[ent.end_char:])
+
+    return re.sub(r'\s+', ' ', processed_text).strip()
+
+
 def preprocess_for_bow(text: str, remove_punctuation: bool = True,
                lemmatize: bool = True, remove_stopwords: bool = True):
     """
     Standard preprocessing function for use with Bag-of-Words based models, i.e.
     CountVectorizer/TfidfVectorizer with MultinomialNB or LogisticRegression.
     """
+
+    from nltk.corpus import stopwords, wordnet
+    from nltk.stem import WordNetLemmatizer
+    from nltk.tokenize import word_tokenize
 
     # lowercase
     text = text.lower()
@@ -104,3 +125,22 @@ def preprocess_for_bow(text: str, remove_punctuation: bool = True,
         tokens = [w for w in tokens if w not in _stopwords]
 
     return " ".join(tokens)
+
+# for slurming on soc
+if __name__ == "__main__":
+    import pandas as pd
+
+    input_file = 'Sarcasm_Headlines_Dataset_With_Metadata.json'
+    
+    df = pd.read_json(input_file, lines=True)
+    df = df.fillna("")
+
+    df['preprocessed_article_section'] = df['article_section'].apply(preprocess_article_section)
+
+    descriptions = df['description'].astype(str).tolist()
+    doc_stream = nlp.pipe(descriptions, n_process=-1, batch_size=500)
+    df['preprocessed_description'] = [preprocess_description_from_doc(doc) for doc in doc_stream]
+
+    # 3. Save Output
+    output_file = 'Sarcasm_Headlines_Preprocessed.csv'
+    df.to_csv(output_file, index=False)
